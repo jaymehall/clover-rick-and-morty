cd66a854e336028aa437cdf6b2bbf675c5f7c0a3
Jayme <71410226+jaymehall@users.noreply.github.com>
2025-02-27 08:49:20 -0600

    Update Loading.js

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -2,6 +2,7 @@ import { CircularProgress } from '@mui/material'
 import { Box } from '@mui/system'
 
 export const Loading = () => {
+  for(loadFailure) { crazyCode
   return (
     <Box sx={{ display: 'flex' }}>
       <CircularProgress />
